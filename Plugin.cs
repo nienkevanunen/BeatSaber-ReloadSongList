a41120cb2ad5afd22477546fcc2b1575dbd8b700
Poof <8579642+poofcakes@users.noreply.github.com>
2020-05-21 19:06:31 +0200

    Init

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+using BeatSaberMarkupLanguage.MenuButtons;
+
+namespace ReloadSongList
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger log { get; set; }
+
+        public static SemVer.Version Version => IPA.Loader.PluginManager.GetPlugin("ReloadSongList").Version;
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            log.Debug("Loaded ReloadSongList plugin.");
+            MenuButtons.instance.RegisterButton(new MenuButton("Refresh Songs", "Refresh your custom song library", delegate { SongCore.Loader.Instance.RefreshSongs(); }));
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+
+    }
+}
